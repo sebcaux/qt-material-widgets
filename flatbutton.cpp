58c40da75632f9021d4080019a70ea4fa068d6ae
lazerpants <johannes@isomorphic.co>
2016-03-18 17:07:20 +0300

    basic button behavior

--- flatbutton.cpp
+++ flatbutton.cpp
@@ -1,3 +1,6 @@
+#include <QDebug>
+#include <QStylePainter>
+#include <QStyleOptionButton>
 #include "flatbutton.h"
 
 FlatButton::FlatButton(QWidget *parent)
@@ -13,4 +16,45 @@ FlatButton::FlatButton(const QString &text, QWidget *parent)
 
 FlatButton::~FlatButton()
 {
+}
+
+void FlatButton::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event)
+
+    QStylePainter painter(this);
+
+    QStyleOptionButton option;
+    option.initFrom(this);
+    option.features = QStyleOptionButton::None;
+    option.features |= QStyleOptionButton::Flat;
+    if (isChecked())
+        option.state |= QStyle::State_On;
+    option.text = text();
+    option.icon = icon();
+    option.iconSize = iconSize();
+
+    painter.drawControl(QStyle::CE_PushButton, option);
+
+    if (underMouse()) {
+        QRect r = rect();
+        QBrush brush;
+        brush.setStyle(Qt::SolidPattern);
+        painter.setOpacity(0.1);
+        painter.fillRect(r, brush);
+    }
+}
+
+void FlatButton::enterEvent(QEvent *event)
+{
+    Q_UNUSED(event)
+
+    update();
+}
+
+void FlatButton::leaveEvent(QEvent *event)
+{
+    Q_UNUSED(event)
+
+    update();
 }
