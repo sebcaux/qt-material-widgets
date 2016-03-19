07f3e183b4e01827fda8045445e9aa641ab1a6a0
Johannes Hilden <hildenjohannes@gmail.com>
2016-03-19 10:51:25 +0300

    remove explicit pointer reference

--- main.cpp
+++ main.cpp
@@ -6,8 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Style *style = new Style;
-    a.setStyle(style);
+    a.setStyle(new Style);
 
     MainWindow w;
     w.show();
