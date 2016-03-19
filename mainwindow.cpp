dd3a473fff783d1a52310a82e04823f920055700
laserpants <johannes@isomorphic.co>
2016-03-19 11:27:32 +0300

    mess around with layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include <QVBoxLayout>
 #include <QPushButton>
+#include <QLabel>
 #include "mainwindow.h"
 #include "flatbutton.h"
 #include "iconbutton.h"
@@ -25,12 +26,22 @@ MainWindow::MainWindow(QWidget *parent)
 
     layout->addWidget(flatButton);
 
+    // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+
+    QHBoxLayout *hLayout = new QHBoxLayout;
+    QLabel *label = new QLabel("Hello");
+    label->setMaximumHeight(32);
+
     IconButton *iconButton = new IconButton(icon);
     iconButton->setText("My button sis afdadsfadsf adsfasdf");
     iconButton->setIconSize(QSize(32, 32));
-    //iconButton->setMaximumWidth(32);
 
-    layout->addWidget(iconButton);
+    hLayout->addWidget(iconButton);
+    hLayout->addWidget(label);
+
+    layout->addLayout(hLayout);
+
+    // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
     QPushButton *button2 = new QPushButton("Test #2");
     layout->addWidget(button2);
