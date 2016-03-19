cf1d873ec30f5916da4670ebed34e4f59f41c248
laserpants <johannes@isomorphic.co>
2016-03-19 13:51:17 +0300

    implement Tab component

--- tab.cpp
+++ tab.cpp
@@ -1,1 +1,18 @@
 #include "tab.h"
+
+Tab::Tab(QWidget *parent)
+    : FlatButton(parent)
+{
+    setMinimumHeight(50);
+}
+
+Tab::Tab(QString text, QWidget *parent)
+    : FlatButton(parent)
+{
+    setMinimumHeight(50);
+    setText(text);
+}
+
+Tab::~Tab()
+{
+}
