a24c010be1e491265a645002ae48feef9e1cc204
laserpants <johannes@isomorphic.co>
2016-03-20 11:56:51 +0300

    implement basic Table component

--- components/table.cpp
+++ components/table.cpp
@@ -1,1 +1,19 @@
 #include "table.h"
+
+Table::Table(QWidget *parent)
+    : QTableWidget(parent)
+{
+    setShowGrid(false);
+    setSelectionMode(QAbstractItemView::SingleSelection);
+    setSelectionBehavior(QAbstractItemView::SelectRows);
+    setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+}
+
+Table::~Table()
+{
+}
+
+bool Table::eventFilter(QObject *obj, QEvent *event)
+{
+    return QTableWidget::eventFilter(obj, event);
+}
