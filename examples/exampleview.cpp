949de87e2556239f1a6399a5df67f2296ff1788a
johanneshilden <hildenjohannes@gmail.com>
2016-03-22 11:19:30 +0300

    refactor ExampleView class

--- examples/exampleview.cpp
+++ examples/exampleview.cpp
@@ -1,13 +1,35 @@
+#include <QEvent>
+#include <QDebug>
 #include "exampleview.h"
 
 ExampleView::ExampleView(QWidget *parent)
-    : QGraphicsView(parent)
+    : QScrollArea(parent),
+      _widget(0)
 {
-    QGraphicsScene *scene = new QGraphicsScene(this);
-    setScene(scene);
 }
 
 ExampleView::~ExampleView()
 {
 }
 
+void ExampleView::setWidget(QWidget *widget)
+{
+    if (_widget)
+        _widget->removeEventFilter(this);
+
+    widget->setParent(this);
+    widget->installEventFilter(this);
+    _widget = widget;
+}
+
+bool ExampleView::eventFilter(QObject *obj, QEvent *event)
+{
+    QEvent::Type type = event->type();
+    if (QEvent::Resize == type || QEvent::Move == type) {
+        QRect r(_widget->geometry());
+        qDebug() << r;
+        r.moveCenter(rect().center());
+        _widget->setGeometry(r);
+    }
+    return QScrollArea::eventFilter(obj, event);
+}
--- examples/exampleview.h
+++ examples/exampleview.h
@@ -1,15 +1,23 @@
 #ifndef EXAMPLEVIEW_H
 #define EXAMPLEVIEW_H
 
-#include <QGraphicsView>
+#include <QScrollArea>
 
-class ExampleView : public QGraphicsView
+class ExampleView : public QScrollArea
 {
     Q_OBJECT
 
 public:
     explicit ExampleView(QWidget *parent = 0);
     ~ExampleView();
+
+    void setWidget(QWidget *widget);
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    QWidget *_widget;
 };
 
 #endif // EXAMPLEVIEW_H
