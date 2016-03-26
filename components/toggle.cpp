401bf7a95a725c63b19b0733a8c69614eded8c63
laserpants <johannes@isomorphic.co>
2016-03-26 13:37:48 +0300

    insert boilerplate class definition

--- components/toggle.cpp
+++ components/toggle.cpp
@@ -1,1 +1,19 @@
+#include <QAbstractButton>
 #include "toggle.h"
+#include "../lib/rippleoverlay.h"
+
+Toggle::Toggle(QWidget *parent)
+    : QAbstractButton(parent),
+      _overlay(new RippleOverlay(this))
+{
+}
+
+Toggle::~Toggle()
+{
+}
+
+void Toggle::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event)
+}
+
--- components/toggle.h
+++ components/toggle.h
@@ -1,4 +1,23 @@
 #ifndef TOGGLE_H
 #define TOGGLE_H
 
+#include <QAbstractButton>
+
+class RippleOverlay;
+
+class Toggle : public QAbstractButton
+{
+    Q_OBJECT
+
+public:
+    explicit Toggle(QWidget *parent = 0);
+    ~Toggle();
+
+protected:
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    RippleOverlay *const _overlay;
+};
+
 #endif // TOGGLE_H
