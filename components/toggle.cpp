3871deda30760dfdc7d600fe7662aaed2e5cd611
laserpants <johannes@isomorphic.co>
2016-03-26 15:20:23 +0300

    remove debug output

--- components/toggle.cpp
+++ components/toggle.cpp
@@ -8,7 +8,6 @@ Toggle::Toggle(QWidget *parent)
     : QAbstractButton(parent),
       _overlay(new RippleOverlay(this))
 {
-    qDebug() << "1";
 }
 
 Toggle::~Toggle()
@@ -19,8 +18,6 @@ void Toggle::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event)
 
-    qDebug() << "2";
-
     QPainter painter(this);
 
     QPen pen;
@@ -29,6 +26,4 @@ void Toggle::paintEvent(QPaintEvent *event)
     painter.setPen(pen);
 
     painter.drawRect(rect());
-
-    painter.drawRect(QRect(10, 10, 30, 30));
 }
