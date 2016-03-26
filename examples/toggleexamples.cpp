e87b786815277c6a035b15f57e54cd3a86308b12
laserpants <johannes@isomorphic.co>
2016-03-26 15:18:06 +0300

    show toggle example

--- examples/toggleexamples.cpp
+++ examples/toggleexamples.cpp
@@ -1,8 +1,30 @@
+#include <QLayout>
 #include "toggleexamples.h"
+#include "components/toggle.h"
+#include "exampleview.h"
+#include "frame.h"
 
 ToggleExamples::ToggleExamples(QWidget *parent)
     : ExampleList(parent)
 {
+    QLayout *layout = widget()->layout();
+
+    {
+        Toggle *toggle = new Toggle;
+
+        ExampleView *view = new ExampleView;
+        view->setWidget(toggle);
+
+        Frame *frame = new Frame;
+        frame->setCodeSnippet(
+            "??"
+        );
+        frame->setWidget(view);
+
+        layout->addWidget(frame);
+
+        toggle->update();
+    }
 }
 
 ToggleExamples::~ToggleExamples()
