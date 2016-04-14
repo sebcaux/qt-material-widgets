a6038ff9f2ad2bc026d7dbae9b6395059ae05ea0
laserpants <johannes@isomorphic.co>
2016-04-14 18:15:17 +0300

    create animation on TextField

--- components/textfield.h
+++ components/textfield.h
@@ -3,18 +3,34 @@
 
 #include <QLineEdit>
 
+class QPropertyAnimation;
+
 class TextField : public QLineEdit
 {
     Q_OBJECT
+
+    Q_PROPERTY(qreal progress WRITE setProgress READ progress NOTIFY progressChanged)
 
 public:
     explicit TextField(QWidget *parent = 0);
     ~TextField();
 
+    void setProgress(qreal progress);
+    inline qreal progress() const { return _progress; }
+
+signals:
+    void progressChanged(qreal);
+
 protected:
+    void focusInEvent(QFocusEvent *event) Q_DECL_OVERRIDE;
+    void focusOutEvent(QFocusEvent *event) Q_DECL_OVERRIDE;
     void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
     void mouseReleaseEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    QPropertyAnimation *const _animation;
+    qreal                     _progress;
 };
 
 #endif // TEXTFIELD_H
