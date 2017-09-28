e6d754a3d60e0aad73106e3c2086492ffedcec72
johanneshilden <hildenjohannes@gmail.com>
2017-09-29 01:29:18 +0300

    Add a left menu to navigate examples editor

--- examples/mainwindow.cpp
+++ examples/mainwindow.cpp
@@ -1,16 +1,44 @@
 #include "mainwindow.h"
-#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QStackedLayout>
+#include <QtWidgets/QListWidget>
 #include "avatarsettingseditor.h"
 #include "badgesettingseditor.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    AvatarSettingsEditor *editor = new AvatarSettingsEditor;
-    setCentralWidget(editor);
+    QWidget *widget = new QWidget;
+    QHBoxLayout *layout = new QHBoxLayout;
 
-    //BadgeSettingsEditor *editor = new BadgeSettingsEditor;
-    //setCentralWidget(editor);
+    widget->setLayout(layout);
+
+    QStackedLayout *stack = new QStackedLayout;
+    QListWidget *list = new QListWidget;
+
+    setCentralWidget(widget);
+
+    layout->addWidget(list);
+    layout->addLayout(stack);
+
+    layout->setStretch(1, 2);
+
+    AvatarSettingsEditor *avatar = new AvatarSettingsEditor;
+    BadgeSettingsEditor *badge = new BadgeSettingsEditor;
+
+    stack->addWidget(avatar);
+    stack->addWidget(badge);
+
+    list->addItem("Avatar");
+    list->addItem("Badge");
+
+    QObject::connect(list,  &QListWidget::currentItemChanged,
+        [=](QListWidgetItem *current, QListWidgetItem *previous)
+    {
+        Q_UNUSED(current)
+        Q_UNUSED(previous)
+        stack->setCurrentIndex(list->currentRow());
+    });
 }
 
 MainWindow::~MainWindow()
