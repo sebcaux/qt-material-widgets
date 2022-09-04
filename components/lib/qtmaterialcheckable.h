b624b88cb72a597d9f26c3a641d7b9281cbc6611
sebcaux <sebastien.caux@uniswarm.eu>
2022-09-04 11:37:36 +0200

    [lib:checkabke] Reviewed header and added properties

--- components/lib/qtmaterialcheckable.h
+++ components/lib/qtmaterialcheckable.h
@@ -9,6 +9,14 @@ class QtMaterialCheckable : public QAbstractButton
 {
     Q_OBJECT
 
+    Q_PROPERTY(QtMaterialCheckable::LabelPosition labelPosition READ labelPosition WRITE setLabelPosition)
+    Q_PROPERTY(QIcon checkedIcon READ checkedIcon WRITE setCheckedIcon)
+    Q_PROPERTY(QIcon uncheckedIcon READ uncheckedIcon WRITE setUncheckedIcon)
+    Q_PROPERTY(QColor checkedColor READ checkedColor WRITE setCheckedColor)
+    Q_PROPERTY(QColor uncheckedColor READ uncheckedColor WRITE setUncheckedColor)
+    Q_PROPERTY(QColor textColor READ textColor WRITE setTextColor)
+    Q_PROPERTY(QColor disabledColor READ disabledColor WRITE setDisabledColor)
+
 public:
     enum LabelPosition
     {
@@ -19,40 +27,27 @@ public:
     explicit QtMaterialCheckable(QWidget *parent = nullptr);
     ~QtMaterialCheckable() override;
 
-    void setLabelPosition(LabelPosition placement);
     LabelPosition labelPosition() const;
+    void setLabelPosition(LabelPosition placement);
 
-    void setUseThemeColors(bool value);
-    bool useThemeColors() const;
-
-    void setCheckedColor(const QColor &color);
-    QColor checkedColor() const;
-
-    void setUncheckedColor(const QColor &color);
-    QColor uncheckedColor() const;
-
-    void setTextColor(const QColor &color);
-    QColor textColor() const;
-
-    void setDisabledColor(const QColor &color);
-    QColor disabledColor() const;
-
-    void setCheckedIcon(const QIcon &icon);
     QIcon checkedIcon() const;
-
-    void setUncheckedIcon(const QIcon &icon);
+    void setCheckedIcon(const QIcon &icon);
     QIcon uncheckedIcon() const;
+    void setUncheckedIcon(const QIcon &icon);
 
-    QSize sizeHint() const override;
+    bool useThemeColors() const;
+    void setUseThemeColors(bool value);
+    QColor checkedColor() const;
+    void setCheckedColor(const QColor &color);
+    QColor uncheckedColor() const;
+    void setUncheckedColor(const QColor &color);
+    QColor textColor() const;
+    void setTextColor(const QColor &color);
+    QColor disabledColor() const;
+    void setDisabledColor(const QColor &color);
 
 protected:
     QtMaterialCheckable(QtMaterialCheckablePrivate &d, QWidget *parent = nullptr);
-
-    // QWidget interface
-    bool event(QEvent *event) override;
-    bool eventFilter(QObject *obj, QEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-    void paintEvent(QPaintEvent *event) override;
 
     virtual void setupProperties();
 
@@ -61,6 +56,16 @@ protected:
 private:
     Q_DISABLE_COPY(QtMaterialCheckable)
     Q_DECLARE_PRIVATE(QtMaterialCheckable)
+
+    // QWidget interface
+public:
+    QSize sizeHint() const override;
+
+protected:
+    bool event(QEvent *event) override;
+    bool eventFilter(QObject *obj, QEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 };
 
 #endif  // QTMATERIALCHECKABLE_H
