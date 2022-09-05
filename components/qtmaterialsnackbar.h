c9fb411fd6c545b4e1e03837ad266745efc5a2c9
sebcaux <sebastien.caux@uniswarm.eu>
2022-09-05 22:08:03 +0200

    [components:snackbar] Reviewed header and added property

--- components/qtmaterialsnackbar.h
+++ components/qtmaterialsnackbar.h
@@ -9,33 +9,37 @@ class QtMaterialSnackbar : public QtMaterialOverlayWidget
 {
     Q_OBJECT
 
+    Q_PROPERTY(bool clickToDismissMode READ clickToDismissMode WRITE setClickToDismissMode)
+    Q_PROPERTY(int autoHideDuration READ autoHideDuration WRITE setAutoHideDuration)
+    Q_PROPERTY(int boxWidth READ boxWidth WRITE setBoxWidth)
+    Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor)
+    Q_PROPERTY(qreal backgroundOpacity READ backgroundOpacity WRITE setBackgroundOpacity)
+    Q_PROPERTY(QColor textColor READ textColor WRITE setTextColor)
+
 public:
     explicit QtMaterialSnackbar(QWidget *parent = nullptr);
     ~QtMaterialSnackbar() override;
 
-    void setAutoHideDuration(int duration);
-    int autoHideDuration() const;
-
-    void setUseThemeColors(bool value);
-    bool useThemeColors() const;
-
-    void setBackgroundColor(const QColor &color);
-    QColor backgroundColor() const;
-
-    void setBackgroundOpacity(qreal opacity);
-    qreal backgroundOpacity() const;
-
-    void setTextColor(const QColor &color);
-    QColor textColor() const;
-
-    void setFontSize(qreal size);
-    qreal fontSize() const;
-
-    void setBoxWidth(int width);
-    int boxWidth() const;
-
-    void setClickToDismissMode(bool value);
     bool clickToDismissMode() const;
+    void setClickToDismissMode(bool value);
+
+    int autoHideDuration() const;
+    void setAutoHideDuration(int duration);
+
+    int boxWidth() const;
+    void setBoxWidth(int width);
+
+    qreal fontSize() const;
+    void setFontSize(qreal size);
+
+    bool useThemeColors() const;
+    void setUseThemeColors(bool value);
+    QColor backgroundColor() const;
+    void setBackgroundColor(const QColor &color);
+    qreal backgroundOpacity() const;
+    void setBackgroundOpacity(qreal opacity);
+    QColor textColor() const;
+    void setTextColor(const QColor &color);
 
 public slots:
     void addMessage(const QString &message);
@@ -45,13 +49,15 @@ protected slots:
     void dequeue();
 
 protected:
-    void paintEvent(QPaintEvent *event) override;
-
     const QScopedPointer<QtMaterialSnackbarPrivate> d_ptr;
 
 private:
     Q_DISABLE_COPY(QtMaterialSnackbar)
     Q_DECLARE_PRIVATE(QtMaterialSnackbar)
+
+    // QWidget interface
+protected:
+    void paintEvent(QPaintEvent *event) override;
 };
 
 #endif  // QTMATERIALSNACKBAR_H
