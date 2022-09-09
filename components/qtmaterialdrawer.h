02c73ea5d85fb7fd11c9dbe4e40e7129baa18776
sebcaux <sebastien.caux@uniswarm.eu>
2022-09-09 23:33:43 +0200

    [components:drawer] Reviewed header and added property

--- components/qtmaterialdrawer.h
+++ components/qtmaterialdrawer.h
@@ -10,39 +10,45 @@ class QtMaterialDrawer : public QtMaterialOverlayWidget
 {
     Q_OBJECT
 
+    Q_PROPERTY(bool clickOutsideToClose READ clickOutsideToClose WRITE setClickOutsideToClose)
+    Q_PROPERTY(bool autoRaise READ autoRaise WRITE setAutoRaise)
+    Q_PROPERTY(bool overlayMode READ overlayMode WRITE setOverlayMode)
+
 public:
     explicit QtMaterialDrawer(QWidget *parent = nullptr);
     ~QtMaterialDrawer() override;
 
-    void setDrawerWidth(int width);
     int drawerWidth() const;
+    void setDrawerWidth(int width);
 
-    void setDrawerLayout(QLayout *layout);
     QLayout *drawerLayout() const;
+    void setDrawerLayout(QLayout *layout);
 
-    void setClickOutsideToClose(bool state);
     bool clickOutsideToClose() const;
+    void setClickOutsideToClose(bool state);
 
-    void setAutoRaise(bool state);
     bool autoRaise() const;
+    void setAutoRaise(bool state);
 
-    void setOverlayMode(bool value);
     bool overlayMode() const;
+    void setOverlayMode(bool value);
 
 public slots:
     void openDrawer();
     void closeDrawer();
 
 protected:
-    bool event(QEvent *event) override;
-    bool eventFilter(QObject *obj, QEvent *event) override;
-    void paintEvent(QPaintEvent *event) override;
-
     const QScopedPointer<QtMaterialDrawerPrivate> d_ptr;
 
 private:
     Q_DISABLE_COPY(QtMaterialDrawer)
     Q_DECLARE_PRIVATE(QtMaterialDrawer)
+
+    // QWidget interface
+protected:
+    bool event(QEvent *event) override;
+    bool eventFilter(QObject *obj, QEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 };
 
 #endif  // QTMATERIALDRAWER_H
